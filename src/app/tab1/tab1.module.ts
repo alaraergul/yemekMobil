543e32f1b1d3534bfd21971b9ab9142a32bd82b9
alaraergul <alaraergul3@gmail.com>
2025-07-07 18:33:51 +0300

    İlk

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,19 +1,18 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
