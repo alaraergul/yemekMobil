49f3a7aaee1c32947b3aa8bcf01faadf481bf774
aloima <acar.aloima@gmail.com>
2025-07-11 13:01:53 +0300

    fix capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
+
 const config: CapacitorConfig = {
   appId: "demo.bussion.purine",
   appName: "Pürin Takibi",
-  bundledWebRuntime: false,
-  npmClient: "npm",
   webDir: "www"
 };
 
