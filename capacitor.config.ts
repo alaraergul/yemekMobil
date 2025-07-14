9d9b013f98d85dd4142ee17d00d27543f02af904
aloima <acar.aloima@gmail.com>
2025-07-14 16:16:37 +0300

    change keyboard mode

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.bussion.purine",
+  appId: "com.bussion.nutrilog",
   appName: "NutriLog",
   webDir: "www"
 };
